e939cf352a61a2f588ed06c5b0e2eb21dbc605e7
Steve Polito <info@stevepolitodesign.com>
2021-03-17 19:35:00 -0400

    Add seed data.
    * rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |i|
+    Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph(sentence_count: 500))
+end
